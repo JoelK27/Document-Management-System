43d94906dd45eea8663be92e6e0f0eeb3823ca85
Elias Zessner-Spitzenberg <elias.zessner@gmail.com>
2025-09-11 14:48:39 +0200

    Added DAL

---
+++ DocumentDAL/src/main/java/at/technikum_wien/DocumentDAL/DocumentDalApplication.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.DocumentDAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentDalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentDalApplication.class, args);
+	}
+
+}
